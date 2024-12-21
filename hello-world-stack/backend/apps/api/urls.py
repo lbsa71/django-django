5e34e47aeae06d6f669b8401bcc7c8cc5535720b
“lbsa71” <lbsa71@lbsa71.net>
2024-12-21 13:41:09 +0100

    fix

--- hello-world-stack/backend/apps/api/urls.py
+++ hello-world-stack/backend/apps/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import HelloWorldView
 
 urlpatterns = [
-    path('api/hello/', HelloWorldView.as_view(), name='hello-world'),
+    path('hello/', HelloWorldView.as_view(), name='hello-world'),
 ]
